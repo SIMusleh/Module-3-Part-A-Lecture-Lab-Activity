fce8ad8af20924eb9b6af4d2b31d2c6a4db570d1
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-13 23:57:41 +0000

    add plant_dynamic.cpp and header

---
+++ plant_dynamic.cpp
@@ -0,0 +1,11 @@
+/* Program name: plant_dynamic.cpp
+ * Author: Sahar Musleh
+ * Date last updated: 11/13/2025
+ * Purpose: Demonstrate dynamic object and dynamic array using Plant class from midterm project.
+ */
+
+ #include <iostream>
+ #include <string>
+ using namespace std;
+
+ 
