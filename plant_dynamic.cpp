0afe0a1d40059a65103d169b3110a96ff65c03c7
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-14 00:08:39 +0000

    add plant class and private and public

--- plant_dynamic.cpp
+++ plant_dynamic.cpp
@@ -8,4 +8,20 @@
  #include <string>
  using namespace std;
 
- 
+class Plant {
+private:
+    string name;
+    double seedCost;
+    double waterPerDay;
+    string season;
+    int squaresNeeded;
+    bool needsWater;
+    int daysPlanted;
+
+public:
+    plant(string n = "Generic Plant", double cost = 1.0, double water = 1.0,
+    string s = "Spring", int squares = 1, bool waterStatus = true, int days = 0)
+    : name(n), seedCost(cost), waterPerDay(water), season(s), squaresNeeded(squares), needsWater(waterStatus), daysPlanted(days) {}
+
+    
+ }
